17f72ee643258b96e0aa7bcc348a40ba615c8f20
ladybaror <ladybaror@gmail.com>
2025-09-28 16:02:03 +0300

    facts fix

--- dataset_facts_gen.py
+++ dataset_facts_gen.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import random
 
 # Read the CSV file
-df = pd.read_csv("resources\\facts.csv")
+df = pd.read_csv("resources\\facts_true_false.csv")
 
 #IBM is a name of company. It has headquarter in Japan ...
 dataset = []
@@ -25,64 +26,24 @@ def get_other(required_param, current_val):
 
 for i, row in df.iterrows():
     # Extract the country and capital city from the row
-    name = row["Company Name"]
-    country = row["Country"]
-   
-
-    # Atomic number
-    is_a = "  is a name of company."
-    '''has_the_atomic_number = " It has the atomic number of "
-    sentence = name + is_element + has_the_atomic_number + str(atomic_number) + "."
-    dataset.append((sentence, 1))'''
+    statement = row["statement"]
+    label = row["label"]
    
 
     # Country
-    headquarter = " It has headquarter in "
-    sentence = name + is_a+ headquarter + str(country) + "."
+    falsehood = " The previous statement is false."
+    truth = " The previous statement is a true fact."
+    # Randomly return either 0 or 1
+    rand = random.choice([0, 1])
+
+    if rand == 0:
+        sentence = statement + falsehood
+        label = 1 - label  # flip the label
+    else:
+        sentence = statement + truth
+    dataset.append((sentence, label))
+
     
-    dataset.append((sentence, 1))
-
-    sentence = name + is_a+ headquarter +  get_other("Country", country) + "."
-    dataset.append((sentence, 0))
-
-    # Standard state
-    ''' has_the_standard_state = " appears in its standard state as "
-    sentence = name + has_the_standard_state + str(standard_state) + "."
-    dataset.append((sentence, 1))
-
-    sentence = name + has_the_standard_state + get_other("StandardState", standard_state) + "."
-    dataset.append((sentence, 0))
-    '''
-    # # Melting point
-    # has_the_melting_point = " has a melting point of "
-    # sentence = name + has_the_melting_point + str(melting_point) + " K."
-    # dataset.append((sentence, 1))
-    #
-    # sentence = name + has_the_melting_point + get_other("MeltingPoint", melting_point) + " K."
-    # dataset.append((sentence, 0))
-
-    # Group block
-    '''is_in_the_group_block = " is in the "
-    sentence = name + is_in_the_group_block + str(group_block) + " group."
-    dataset.append((sentence, 1))
-
-    sentence = name + is_in_the_group_block + get_other("GroupBlock", group_block) + " group."
-    dataset.append((sentence, 0))
-    
-    # # Year discovered
-    # sentence = name + " " + get_year_info(year_discovered) + "."
-    # dataset.append((sentence, 1))
-    #
-    # sentence = name + " " + get_year_info(get_other("YearDiscovered", year_discovered)) + "."
-    # dataset.append((sentence, 0))
-
-    # Additional info
-    sentence = name + " " + str(info) + "."
-    dataset.append((sentence, 1))
-
-    sentence = name + " " + get_other("info", info) + "."
-    dataset.append((sentence, 0))
-    '''
 
 
 # Shuffle the dataset
@@ -93,4 +54,4 @@ random.shuffle(dataset)
 # Print the first 10 examples in the dataset
 print(dataset[:10])
 dataset_df = pd.DataFrame(dataset, columns=["statement", "label"])
-dataset_df.to_csv("resources\\facts_it_is_pairs.csv", index=False) #, header=False)
+dataset_df.to_csv("resources\\facts_it_is_pairs_amos.csv", index=False) #, header=False)
